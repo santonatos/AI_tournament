9d0a32d2f935fd6e93ad89d88901057fb82c4b14
Chad Underhill <chad.j.underhill@gmail.com>
2017-10-07 15:53:34 -0400

    Evaluation function re-built. Still untested

--- headers/Evaluation.hpp
+++ headers/Evaluation.hpp
@@ -4,6 +4,17 @@
 #include <queue>
 #include <time>
 #include <random>
+#include <tuple>
 
+tuple firstMove(Board board);
+tuple secondMove(Board board);
+tuple randomMove(Board board);
 
-#end
+int evalFunction(Board board, tuple position, bool mode);
+int evaluatePosition(Board board, tuple position);
+list<tuple> attackArea(tuple initPair, int connect);
+
+tuple topMoves(Board board, int limit);
+tuple evaluationFunction(Board board);
+
+#endif
